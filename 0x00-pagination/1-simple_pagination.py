57b13f72723e2b5ab87175d95412532eb6398f1c
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-01-29 12:03:09 +0100

    pagination task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+
+    Args:
+        page (int): Page number
+        page_size (int): Number of items per page
+
+    Returns:
+        Tuple: tuple containing start and end indices
+    """
+    # Calculate the start index
+    start_index = (page - 1) * page_size
+
+    # Calculate the end index
+    end_index = start_index + page_size
+
+    return start_index, end_index
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data
+
+        Args:
+            page (int, optional): Page Number. Defaults to 1.
+            page_size (int, optional): Number of items per page.
+                Defaults to 10.
+
+        Returns:
+            List[List]: Page of data
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        i_range = index_range(page, page_size)
+        start_index, end_index = i_range
+        dataset = self.dataset()
+        result = []
+
+        if dataset is not None and end_index < len(dataset):
+            for i in range(start_index, end_index):
+                result.append(dataset[i])
+        return result
