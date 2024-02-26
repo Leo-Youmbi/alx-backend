9adc3482672259a77763648fdae16acc622cbaf6
Leo-Youmbi <leoyoumbi83@gmail.com>
2024-02-26 14:07:10 +0100

    i18n task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""A Basic Flask app.
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
